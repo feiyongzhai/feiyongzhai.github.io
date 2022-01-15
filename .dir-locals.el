8cfa486e463440386021ca0144d2e3370dd95018
yongfei-mint <yongfeizhai@outlook.com>
2022-01-15 21:13:15 +0800

    修改导航栏名字

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,7 @@
 <div id=\"navList\">
 <ul>
 <li><a href=\"./index.html\">首页</a></li>
-<li><a href=\"./emacs.html\">Emacs</a></li>
+<li><a href=\"./blog.html\">日志</a></li>
 <li><a href=\"./about.html\">关于</a></li>
 </ul>
 </div>
