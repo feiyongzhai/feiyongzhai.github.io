dba368d7f2074f03b351a979998693139ef373fb
yongfei-mint <yongfeizhai@outlook.com>
2022-01-15 20:27:08 +0800

    Add preamable
    折腾的过程中发现了 org-publish ，对比我用 org-export 的手段，有一个比
    较明显的好处是不需要每个文件都导出 HTML 一下，一个命令就完事了。后续可
    能会考虑用 org-publish 来管理我的博客。

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((org-mode
+  (org-html-preamble-format . (("en"
+				"
+<div id=\"navList\">
+<ul>
+<li><a href=\"./index.html\">首页</a></li>
+<li><a href=\"./about.html\">关于</a></li>
+</ul>
+</div>
+")))))
